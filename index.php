850e794cf3015c1133523a0b2cebd0f67929d28d
aka <aka@startext.dev>
2023-04-22 12:21:38 +0700

    Add index.php.

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Лабораторные работы</title>
+
+    <style>
+        .title {
+            text-align: center;
+            border-bottom: solid;
+            padding-bottom: 20px;
+        }
+
+        .menu {
+            margin-top: 50px;
+        }
+
+        .c {
+            border: 1px solid rgb(51, 51, 51);
+            display: inline-block;
+            padding: 10px 20px;
+            margin: 10px 20px;
+            text-decoration: none;
+            color: rgb(0, 0, 0);
+        }
+
+        .c:hover {
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+            background: linear-gradient(to bottom, rgb(252, 255, 244), rgb(233, 233, 206));
+            color: #a00;
+        }
+    </style>
+</head>
+<body>
+    <div class="title">
+        <h1 style="font-variant-caps: normal">
+            Лабораторные работы
+        </h1>
+        <h3>
+            Выполнил: Карманов Арсений Викторович г. 571-2
+        </h3>
+    </div>
+    <div class="menu">
+
+    </div>
+</body>
+</html>
 <?php
